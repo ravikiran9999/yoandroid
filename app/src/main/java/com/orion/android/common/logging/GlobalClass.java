f63cf72f5c1b2f75326c83ab36e6ad0e64f80c18
Ramesh <ramesh.akula@mtuity.com>
2016-06-20 15:51:21 +0530

    AWS logs and sonar set up has been done.

---
+++ app/src/main/java/com/orion/android/common/logging/GlobalClass.java
@@ -0,0 +1,17 @@
+package com.orion.android.common.logging;
+
+
+/**
+ * @author :paradigmcreatives
+ */
+public class GlobalClass {
+
+    public static final boolean DEBUG = true;
+    public static final String BUCKET_NAME = "cimparking";
+    public static final String LOG_FB_USER_NAME = "AWS_LOGS";
+
+    private GlobalClass(){
+        //default constructor needed
+    }
+
+}
