4bc5a550503ba479af8a65614af4de1149a1f647
Ramesh <ramesh.akula@mtuity.com>
2016-06-28 18:40:35 +0530

    Animation and delete number functionaliy added.

--- app/src/main/java/com/yo/android/ui/DialerActivity.java
+++ app/src/main/java/com/yo/android/ui/DialerActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.yo.android.R;
@@ -16,6 +19,7 @@ import com.yo.android.voip.SipService;
  */
 public class DialerActivity extends BaseActivity {
     private DialPadView dialPadView;
+    private ImageButton deleteButton;
     private static final int[] mButtonIds = new int[]{R.id.zero, R.id.one, R.id.two, R.id.three,
             R.id.four, R.id.five, R.id.six, R.id.seven, R.id.eight, R.id.nine, R.id.star,
             R.id.pound};
@@ -36,6 +40,7 @@ public class DialerActivity extends BaseActivity {
         dialPadView = (DialPadView) findViewById(R.id.dialPadView);
         btnCallGreen = (FloatingActionButton) findViewById(R.id.btnCall);
         btnDialer = (FloatingActionButton) findViewById(R.id.btnDialer);
+        deleteButton = (ImageButton) findViewById(R.id.deleteButton);
         for (int id : mButtonIds) {
             dialPadView.findViewById(id).setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -60,28 +65,75 @@ public class DialerActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 hideDialPad();
-                //TODO: Navigate to next scren
                 String number = dialPadView.getDigits().getText().toString();
-                Intent intent = new Intent(DialerActivity.this, OutGoingCallActivity.class);
-                intent.putExtra(OutGoingCallActivity.CALLER_NO, number);
-                startActivity(intent);
+                if (number.length() > 0) {
+                    Intent intent = new Intent(DialerActivity.this, OutGoingCallActivity.class);
+                    intent.putExtra(OutGoingCallActivity.CALLER_NO, number);
+                    startActivity(intent);
+                }
             }
         });
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String prev = dialPadView.getDigits().getText().toString();
+                String finalString = prev;
+                if (prev.length() > 0) {
+                    finalString = new StringBuilder(prev).deleteCharAt(prev.length() - 1).toString();
+                }
+                dialPadView.getDigits().setText(finalString);
+                dialPadView.getDigits().setSelection(finalString.length());
 
+            }
+        });
     }
 
     private void showDialPad() {
         show = true;
-        dialPadView.setVisibility(View.VISIBLE);
-        btnCallGreen.setVisibility(View.VISIBLE);
-        btnDialer.setVisibility(View.GONE);
+        Animation bottomUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_up);
+        dialPadView.startAnimation(bottomUp);
+        bottomUp.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                dialPadView.setVisibility(View.VISIBLE);
+                btnCallGreen.setVisibility(View.VISIBLE);
+                btnDialer.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        bottomUp.start();
     }
 
     private void hideDialPad() {
         show = false;
-        dialPadView.setVisibility(View.GONE);
-        btnCallGreen.setVisibility(View.GONE);
-        btnDialer.setVisibility(View.VISIBLE);
+        Animation bottomUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_down);
+        dialPadView.startAnimation(bottomUp);
+        bottomUp.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                dialPadView.setVisibility(View.GONE);
+                btnCallGreen.setVisibility(View.GONE);
+                btnDialer.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        bottomUp.start();
     }
 
     @Override
