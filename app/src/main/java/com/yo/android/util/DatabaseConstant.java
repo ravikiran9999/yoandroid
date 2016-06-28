ce15f8ba69100432f072b0808bad2ffd75c42443
ravikiran <ravi.doddapaneni@mtuity.com>
2016-06-28 19:14:56 +0530

    Implemented read and create operations on Ormlite

---
+++ app/src/main/java/com/yo/android/util/DatabaseConstant.java
@@ -0,0 +1,12 @@
+package com.yo.android.util;
+
+public class DatabaseConstant {
+
+    // Chat
+    public static final String MESSAGE = "message";
+    public static final String MESSAGE_ID = "message_id";
+
+    public static final String LOGED_IN_USER_ID = "logedInUserId";
+    public static final String TYPE = "type";
+
+}
