2b75b6dd9d9e9e30f7a95ec8afbfee266cf14652
Ramesh <ramesh.akula@mtuity.com>
2016-07-14 12:04:27 +0530

    Network resonse caching logic added.

--- app/src/main/java/com/yo/android/di/NetWorkModule.java
+++ app/src/main/java/com/yo/android/di/NetWorkModule.java
@@ -1,16 +1,25 @@
 package com.yo.android.di;
 
+import android.content.Context;
+
 import com.orion.android.common.logger.Log;
 import com.orion.android.common.preferences.PreferenceEndPoint;
 import com.yo.android.api.YoApi;
 import com.yo.android.vox.VoxApi;
+
+import java.io.File;
+import java.io.IOException;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -34,9 +43,12 @@ public class NetWorkModule {
 
     @Singleton
     @Provides
-    YoApi.YoService provideYoService(YoApi.YoRefreshTokenService tokenService, @Named("login") PreferenceEndPoint endPoint, Log log) {
+    YoApi.YoService provideYoService(Context context, YoApi.YoRefreshTokenService tokenService, @Named("login") PreferenceEndPoint endPoint, Log log) {
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         builder.authenticator(new TokenAuthenticator(tokenService, endPoint, log));
+        long SIZE_OF_CACHE = 10 * 1024 * 1024; // 10 MB
+        Cache cache = new Cache(new File(context.getCacheDir(), "http"), SIZE_OF_CACHE);
+        builder.cache(cache);
         return buildAdapter(YoApi.BASE_URL, YoApi.YoService.class, builder);
     }
 
@@ -50,8 +62,12 @@ public class NetWorkModule {
     private <T> T buildAdapter(String baseUrl, Class<T> clazz, OkHttpClient.Builder builder) {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        //
+
         OkHttpClient defaultHttpClient = builder
                 .addInterceptor(interceptor)
+                //work like charm! Enable later
+//                .addNetworkInterceptor(mCacheControlInterceptor)
                 .build();
         Retrofit retrofit = new Retrofit.Builder()
                 .client(defaultHttpClient)
@@ -60,4 +76,35 @@ public class NetWorkModule {
                 .build();
         return retrofit.create(clazz);
     }
+
+    //https://gist.github.com/polbins/1c7f9303d2b7d169a3b1
+    private static final Interceptor mCacheControlInterceptor = new Interceptor() {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+
+            // Add Cache Control only for GET methods
+            if (request.method().equals("GET")) {
+                if (false/*ConnectivityHelper.isNetworkAvailable(mContext)*/) {
+                    // 1 day
+                    request.newBuilder()
+                            .header("Cache-Control", "only-if-cached")
+                            .build();
+                } else {
+                    // 4 weeks stale
+                    request.newBuilder()
+                            .header("Cache-Control", "public, max-stale=2419200")
+                            .build();
+                }
+            }
+
+            Response response = chain.proceed(request);
+
+            // Re-write response CC header to force use of cache
+            return response;
+//            .newBuilder()
+//                    .header("Cache-Control", "public, max-age=86400") // 1 day
+//                    .build();
+        }
+    };
 }
