48320fd53bac1de69e4361af126569a69e1bfb3c
ravikiran <ravi.doddapaneni@mtuity.com>
2016-07-18 14:53:07 +0530

    Designed multiple select contacts in group chat

--- app/src/main/java/com/yo/android/chat/ui/CreateGroupActivity.java
+++ app/src/main/java/com/yo/android/chat/ui/CreateGroupActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.yo.android.R;
+import com.yo.android.chat.ui.fragments.GroupContactsFragment;
 import com.yo.android.ui.BaseActivity;
 
 public class CreateGroupActivity extends BaseActivity implements View.OnClickListener{
@@ -16,12 +18,17 @@ public class CreateGroupActivity extends BaseActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_chat_group);
-        ImageView addContactIcon = (ImageView) findViewById(R.id.add_contact);
+        TextView addContactIcon = (TextView) findViewById(R.id.add_contact);
         addContactIcon.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        GroupContactsFragment groupContactsFragment = new GroupContactsFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(android.R.id.content, groupContactsFragment)
+                .commit();
+        enableBack();
     }
 }
