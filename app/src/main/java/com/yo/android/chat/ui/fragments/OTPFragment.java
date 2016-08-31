88e4aba67d45be0d9a02542c6b90f709d453ab1d
IngridCooper <marycooper.ingrid@gmail.com>
2016-08-31 15:31:13 +0530

    Fixed the issue of user getting navigated to landing screen when payment is not made and displaying respective messages for voucher failure

--- app/src/main/java/com/yo/android/chat/ui/fragments/OTPFragment.java
+++ app/src/main/java/com/yo/android/chat/ui/fragments/OTPFragment.java
@@ -29,6 +29,7 @@ import com.yo.android.model.OTPResponse;
 import com.yo.android.model.Subscriber;
 import com.yo.android.pjsip.YoSipService;
 import com.yo.android.ui.BottomTabsActivity;
+import com.yo.android.ui.FollowMoreTopicsActivity;
 import com.yo.android.ui.UpdateProfileActivity;
 import com.yo.android.util.Constants;
 import com.yo.android.voip.IncomingSmsReceiver;
@@ -242,6 +243,7 @@ public class OTPFragment extends BaseFragment {
         contactsSyncManager.syncContacts();
         //
         final boolean isNewUser = preferenceEndPoint.getBooleanPreference("isNewUser");
+        final boolean balanceAdded = preferenceEndPoint.getBooleanPreference("balanceAdded");
         if (isNewUser) {
             //TODO:Enable flag for Profile
             preferenceEndPoint.saveBooleanPreference(Constants.ENABLE_PROFILE_SCREEN, true);
@@ -250,6 +252,15 @@ public class OTPFragment extends BaseFragment {
             intent.putExtra(Constants.PHONE_NUMBER, phoneNumber);
             dismissProgressDialog();
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else if(!balanceAdded) {
+            preferenceEndPoint.saveBooleanPreference(Constants.ENABLE_PROFILE_SCREEN, false);
+            preferenceEndPoint.saveBooleanPreference(Constants.ENABLE_FOLLOW_TOPICS_SCREEN, true);
+            preferenceEndPoint.saveBooleanPreference(Constants.LOGED_IN, true);
+            preferenceEndPoint.saveBooleanPreference(Constants.LOGED_IN_AND_VERIFIED, true);
+            Intent intent = new Intent(getActivity(), FollowMoreTopicsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.putExtra("From", "UpdateProfileActivity");
             startActivity(intent);
         } else {
             startActivity(new Intent(getActivity(), BottomTabsActivity.class));
