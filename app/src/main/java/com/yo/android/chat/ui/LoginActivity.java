cd76eb0123a01edfb2527fb90c18d5d1d02a10b7
ramya <ramyaraju92@gmail.com>
2017-01-23 13:36:47 +0530

    Added Intent package.

--- app/src/main/java/com/yo/android/chat/ui/LoginActivity.java
+++ app/src/main/java/com/yo/android/chat/ui/LoginActivity.java
@@ -3,6 +3,7 @@ package com.yo.android.chat.ui;
 import android.app.AlertDialog;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentTransaction;
