9818f3b4808f4b3b7fff1cee1ae4ee42a49797f3
IngridCooper <marycooper.ingrid@gmail.com>
2017-08-04 17:35:14 +0530

    Implemented new OTP screen similar to Lyft app OTP screen

---
+++ app/src/main/java/com/yo/android/ui/NewOTPActivity.java
@@ -0,0 +1,15 @@
+package com.yo.android.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yowatsup.android.R;
+
+public class NewOTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_otp);
+    }
+}
---
+++ app/src/main/java/com/yo/android/ui/fragments/NewOTPFragment.java
@@ -0,0 +1,8 @@
+package com.yo.android.ui.fragments;
+
+/**
+ * Created by creatives on 7/31/2017.
+ */
+
+public class NewOTPFragment {
+}
